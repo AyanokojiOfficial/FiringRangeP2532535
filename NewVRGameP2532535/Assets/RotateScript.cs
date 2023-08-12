fae5f1c9101bb59ae034401f43eaba472d81c6e8
Mark-Anthony Leonard <Markaleonardbusiness@gmail.com>
2023-08-12 01:15:23 +0100

    Map Updated
    I've updated the map to add more ammo.

---
+++ NewVRGameP2532535/Assets/RotateScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    [SerializeField] private Vector3 _rotation;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(_rotation * Time.deltaTime);
+    }
+}
